18cb670cd43f9ee3dc9b51a1657bcbfaca50db67
aridra <aridrachakraborty0973@gmail.com>
2023-03-19 12:08:42 +0530

    first git commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    
+    <?php
+    $xyz = "My first PHP script!";
+    ?>
+
+    <h1>Welcome!
+        <?php
+        echo $xyz;
+        ?>
+    </h1>
+
+    <?php
+    echo "My first PHP script!";
+    ?>
+
+</body>
+
+</html>
